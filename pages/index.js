0a1c80c2f68b798501f8e989d90f368b250f4a3c
Giulia Borges de Oliveira <64155751+gii-pan@users.noreply.github.com>
2021-01-25 21:23:36 -0300

    magic girl theme

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,6 @@ const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
-const BackgroundImage = styled.div` 
-background-image: url(${db.bg});
-flex:1;
-background-size: cover;
-background-position: center;
-`;
 
 export const QuizContainer = styled.div`
   width: 100%;
